6f6539915df9d165819e9cbbece787cf324262a9
petrero <petrero@gmail.com>
2022-09-18 10:08:42 +0300

    2.14 Add loading progress indicator

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,9 @@
                     <a href="#">
                         <img src="/ff7.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
-                    <div class="absolute"></div>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
                 </div>
                 <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
                     Final Fantasy 7 Remake
