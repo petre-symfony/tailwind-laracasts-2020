446b5881911fb95698c2ac18202cbb1f0171d795
petrero <petrero@gmail.com>
2022-09-18 09:38:53 +0300

    2.7 Add svg icon before search input

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,11 @@
                         "
                         placeholder="Search..."
                     >
+                    <div class="absolute top-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                        </svg>
+                    </div>
                 </div>
                 <div class="ml-6">
                     <a href=""><img src="/avatar.jpg" alt="avatar" class="rounded-full w-8"></a>
