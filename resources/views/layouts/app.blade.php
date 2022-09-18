b4b7f59e7e06b74bc9abba1a2258f292cddfb220
petrero <petrero@gmail.com>
2022-09-18 09:03:14 +0300

    2.3 Create another index page instead of welcome.blade.php with tailwind

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel Video Games</title>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body class="bg-gray-900 text-white">
+    <header class="border-b border-gray-800">
+        <nav class="container mx-auto flex items-center justify-between px-4 py-6">
+            <div class="flex items-center">
+                <a href="">
+                    <img src="/laracasts-logo.svg" alt="laracasts" class="w-32 flex-none">
+                </a>
+                <ul class="flex">
+                    <li><a href="" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="" class="hover:text-gray-400">Reviews</a></li>
+                    <li><a href="" class="hover:text-gray-400">Coming Soon</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    </body>
+</html>
