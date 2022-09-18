b556dba101e762e9ce47e1f711885006bde885c4
petrero <petrero@gmail.com>
2022-09-18 09:49:55 +0300

    2.10 Add a footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,5 +45,15 @@
             </div>
         </nav>
     </header>
+
+    <main class="py-8">
+        @yield('content')
+    </main>
+
+    <footer class="border-t border-gray-800">
+        <div class="container mx-auto px-4 py-6">
+            Powered By <a href="" class="underline hover:text-gray-400">IGDB API</a>
+        </div>
+    </footer>
     </body>
 </html>
