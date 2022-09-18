1da980f6b9a18bdd969b80de32198d90ca9f411e
petrero <petrero@gmail.com>
2022-09-18 09:53:17 +0300

    2.11 Add slot for displaying games

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    index here
+    <div class="container mx-auto px-4">
+        <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular games</h2>
+        <div class="popular-games text-sm grid grid-cols-6 gap-12 border-b border-gray-800">
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="/ff7.jpg" alt="game cover">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
