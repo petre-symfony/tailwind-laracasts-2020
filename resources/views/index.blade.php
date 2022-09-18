6a07a0d13cf778fa519673a52f1e28d96c57d9bb
petrero <petrero@gmail.com>
2022-09-18 12:03:37 +0300

    2.20 Add name and system in the second section of the index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -109,13 +109,22 @@
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
                 <div class="recently-reviewed-container space-y-12 mt-8">
                     <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
-                        <div class="relative inline-block">
+                        <div class="relative flex-none">
                             <a href="#">
                                 <img src="/ff7.jpg" alt="game cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
                             </a>
                             <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right: -20px; bottom: -20px">
                                 <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
                             </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
+                                Final Fantasy 7 Remake
+                            </a>
+                            <div class="text-gray-400 mt-1">Playstation 4</div>
+                            <p class="mt-6 text-gray-400">
+                                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.
+                            </p>
                         </div>
                     </div>
                 </div>
