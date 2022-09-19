3ee71cb47bcb051b742b3a874572b4566aac32ff
petrero <petrero@gmail.com>
2022-09-19 11:55:15 +0300

    4.1 Create detail game page

---
+++ resources/views/show.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mx-auto px-4">
+        <div class="game-details border-b border-gray-800 pb-12 flex">
+            <div class="flex-none">
+                <img src="/ff7.jpg" alt="cover">
+            </div>
+            <div class="ml-12">
+                <h2 class="font-semibold">Final Fantasy 4 Remake</h2>
+            </div>
+        </div>
+    </div>
+@endsection
