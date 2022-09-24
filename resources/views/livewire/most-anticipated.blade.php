3fe2ae164b267045c461eab53573339d97279bec
petrero <petrero@gmail.com>
2022-09-24 21:47:58 +0300

    8.14 Four most-anticipated slots in skeleton loader of the most-anticipated livewire component

--- resources/views/livewire/most-anticipated.blade.php
+++ resources/views/livewire/most-anticipated.blade.php
@@ -10,6 +10,7 @@
             </div>
         </div>
     @empty --}}
+        @foreach(range(1, 4) as $game)
         <div class="game flex">
             <div class="bg-gray-800 w-16 h-20 flex-none"></div>
             <div class="ml-4">
@@ -17,5 +18,6 @@
                 <div class="text-transparent bg-gray-700 rounded inline-block text-sm mt-4">Sept 14, 2022</div>
             </div>
         </div>
+        @endforeach
     {{-- @endforelse --}}
 </div>
