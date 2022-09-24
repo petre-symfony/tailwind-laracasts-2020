6c7b5f12b4a8334133eb6451f7e2c9c7e9cb880e
petrero <petrero@gmail.com>
2022-09-24 20:59:17 +0300

    7.8 Use cache to increase the performance of our application

--- app/Http/Livewire/PopularGames.php
+++ app/Http/Livewire/PopularGames.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
@@ -13,22 +14,26 @@ class PopularGames extends Component {
         $before = Carbon::now()->subMonths(2)->timestamp;
         $after = Carbon::now()->addMonths(2)->timestamp;
 
-        $this->popularGames = Http::withHeaders(config('services.igdb'))
-            ->withBody(
-                "fields name, cover.url, first_release_date, total_rating_count, platforms.abbreviation, rating, slug;
-                where platforms = (48, 49, 130, 6)
-                & (first_release_date > {$before}
-                & first_release_date < {$after}
-                & cover != null
-                & total_rating_count > 5
-                );
-                sort total_rating_count;
-                limit 12;",
-                "text/plain"
-            )
-            ->post('https://api.igdb.com/v4/games')
-            ->json()
-        ;
+        $this->popularGames = Cache::remember('popular-games', 7, function() use ($before, $after){
+            //sleep(3);
+            return Http::withHeaders(config('services.igdb'))
+                ->withBody(
+                    "fields name, cover.url, first_release_date, total_rating_count, platforms.abbreviation, rating, slug;
+                    where platforms = (48, 49, 130, 6)
+                    & (first_release_date > {$before}
+                    & first_release_date < {$after}
+                    & cover != null
+                    & total_rating_count > 5
+                    );
+                    sort total_rating_count;
+                    limit 12;",
+                    "text/plain"
+                )
+                ->post('https://api.igdb.com/v4/games')
+                ->json()
+            ;
+        });
+
     }
 
     public function render() {
