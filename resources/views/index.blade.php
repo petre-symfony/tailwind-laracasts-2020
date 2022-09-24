c1fae1c0afb2214080a2976e298d770bff403978
petrero <petrero@gmail.com>
2022-09-24 18:34:57 +0300

    6.4. Get the correct size of the images in our template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
-                        <img src="{{ $game['cover']['url'] }}" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+                        <img src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
                     <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"  style="right: -20px; bottom: -20px">
                         <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
