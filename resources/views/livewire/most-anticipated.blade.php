60d9c5738e3e35edcfc2cd86b9ebc442fb1641ea
petrero <petrero@gmail.com>
2022-09-24 21:49:21 +0300

    8.15 Uncomment commented block of code in most-anticipated livewire component

--- resources/views/livewire/most-anticipated.blade.php
+++ resources/views/livewire/most-anticipated.blade.php
@@ -1,5 +1,5 @@
 <div wire:init="loadMostAnticipated" class="most-anticipated-container space-y-10 mt-8">
-    {{-- @forelse($mostAnticipatedGames as $game)
+    @forelse($mostAnticipatedGames as $game)
         <div class="game flex">
             <a href="#">
                 <img src="{{ Str::replaceFirst('thumb', 'cover_small', $game['cover']['url']) }}" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
@@ -9,7 +9,7 @@
                 <div class="text-gray-400 text-sm mt-1">{{ \Carbon\Carbon::parse($game['first_release_date'])->format('M d, Y') }}</div>
             </div>
         </div>
-    @empty --}}
+    @empty
         @foreach(range(1, 4) as $game)
         <div class="game flex">
             <div class="bg-gray-800 w-16 h-20 flex-none"></div>
@@ -19,5 +19,5 @@
             </div>
         </div>
         @endforeach
-    {{-- @endforelse --}}
+    @endforelse
 </div>
