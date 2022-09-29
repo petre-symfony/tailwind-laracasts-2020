f74044df4a72a27e65d79cfccaadbd20a4366532
petrero <petrero@gmail.com>
2022-09-29 17:27:47 +0300

    16.2 Mock some divs that shows like we find some games that meet the serach term

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -16,5 +16,19 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
         </div>
+
+        <div class="absolute z-50 bg-gray-800 text-xs rounded w-64 mt-2">
+            <ul>
+                <li>
+                    <a href="" class="block hover:bg-gray-700 px-3 py-3">Game 1</a>
+                </li>
+                <li>
+                    <a href="" class="block hover:bg-gray-700 px-3 py-3">Game 1</a>
+                </li>
+                <li>
+                    <a href="" class="block hover:bg-gray-700 px-3 py-3">Game 1</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </div>
