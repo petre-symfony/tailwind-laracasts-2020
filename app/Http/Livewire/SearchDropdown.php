838c17ea7803c8a9c488359d4dfa47926e6cf682
petrero <petrero@gmail.com>
2022-09-29 17:16:52 +0300

    16.1 Move dropdown search input into it's own livewire component

---
+++ app/Http/Livewire/SearchDropdown.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SearchDropdown extends Component {
+    public function render() {
+        return view('livewire.search-dropdown');
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,24 +22,8 @@
                 </ul>
             </div>
             <div class="flex items-center mt-6 lg:mt-0">
-                <div class="relative">
-                    <input type="text"
-                        class="
-                            bg-gray-800 text-sm rounded-full px-3 py-1
-                            focus:outline-none focus:ring w-64 pl-8
-                        "
-                        placeholder="Search..."
-                    >
-                    <div class="absolute top-0 flex items-center h-full ml-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor"
-                            class="text-gray-400 w-4"
-                        >
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                        </svg>
-                    </div>
-                </div>
+
+                <livewire:search-dropdown />
                 <div class="ml-6">
                     <a href=""><img src="/avatar.jpg" alt="avatar" class="rounded-full w-8"></a>
                 </div>
