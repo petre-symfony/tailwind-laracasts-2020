1e196dd9f3c24bd676eab84134f1025f3ff5d346
petrero <petrero@gmail.com>
2022-10-02 09:17:28 +0300

    18.5 Initial modal for screenshots

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -163,6 +163,22 @@
                 </div>
                 @endforeach
             </div>
+            <div class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+                 style="background-color: rgba(0, 0, 0, .5 )"
+            >
+                <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                    <div class="bg-gray-900 rounded">
+                        <div class="flex justify-end pr-4 pt-2">
+                            <button
+                                class="text-3xl leading-none hover:text-gray-300"
+                            >&times;</button>
+                        </div>
+                        <div class="modal-body px-8 py-8">
+                            img goes here
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div><!-- end images container -->
         <div class="similar-games-container mt-8">
             <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Similar Games</h2>
