92903b2db9b0c59551a8fe72c5a1eee5e4d362db
petrero <petrero@gmail.com>
2022-10-02 08:44:46 +0300

    18.1 Write the html for a modal

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -117,6 +117,30 @@
                         </svg>
                         <span class="ml-2">Play Trailer</span>
                     </a>
+                    <div class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+                        style="background-color: rgba(0, 0, 0, .5 )"
+                    >
+                        <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                            <div class="bg-gray-900 rounded">
+                                <div class="flex justify-end pr-4 pt-2">
+                                    <button class="text-3xl leading-none hover:text-gray-300">&times;</button>
+                                </div>
+                                <div class="modal-body px-8 py-8">
+                                    <div class="responsive-container overflow-hidden relative"
+                                        style="padding-top: 56.25%"
+                                    >
+                                        <iframe src="https://youtube.com/embed/D9oIu6jiYLk"
+                                            class="responsive-iframe absolute top-0 left-0 w-full h-full"
+                                            width="560" height="315"
+                                            style="border: 0;" allow="autoplay; encrypted-media"
+                                            allowfullscreen
+                                        >
+                                        </iframe>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div><!-- end game details -->
