ec16b9e7de7c78b77fbc647aa7be6ea0e5acb992
petrero <petrero@gmail.com>
2022-10-02 09:12:53 +0300

    18.4 Stop the youtube player to play after we close the modal

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -119,35 +119,36 @@
                         <span class="ml-2">Play Trailer</span>
                     </a> --}}
 
-                    <div class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
-                        style="background-color: rgba(0, 0, 0, .5 )"
-                        x-show="isTrailerModalVisible"
-                    >
-                        <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
-                            <div class="bg-gray-900 rounded">
-                                <div class="flex justify-end pr-4 pt-2">
-                                    <button
-                                        class="text-3xl leading-none hover:text-gray-300"
-                                        @click="isTrailerModalVisible = false"
-                                        @keydown.escape.window="isTrailerModalVisible = false"
-                                    >&times;</button>
-                                </div>
-                                <div class="modal-body px-8 py-8">
-                                    <div class="responsive-container overflow-hidden relative"
-                                        style="padding-top: 56.25%"
-                                    >
-                                        <iframe src="{{ $game['trailer'] }}"
-                                            class="responsive-iframe absolute top-0 left-0 w-full h-full"
-                                            width="560" height="315"
-                                            style="border: 0;" allow="autoplay; encrypted-media"
-                                            allowfullscreen
+                    <template x-if="isTrailerModalVisible">
+                        <div class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+                            style="background-color: rgba(0, 0, 0, .5 )"
+                        >
+                            <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                                <div class="bg-gray-900 rounded">
+                                    <div class="flex justify-end pr-4 pt-2">
+                                        <button
+                                            class="text-3xl leading-none hover:text-gray-300"
+                                            @click="isTrailerModalVisible = false"
+                                            @keydown.escape.window="isTrailerModalVisible = false"
+                                        >&times;</button>
+                                    </div>
+                                    <div class="modal-body px-8 py-8">
+                                        <div class="responsive-container overflow-hidden relative"
+                                            style="padding-top: 56.25%"
                                         >
-                                        </iframe>
+                                            <iframe src="{{ $game['trailer'] }}"
+                                                class="responsive-iframe absolute top-0 left-0 w-full h-full"
+                                                width="560" height="315"
+                                                style="border: 0;" allow="autoplay; encrypted-media"
+                                                allowfullscreen
+                                            >
+                                            </iframe>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </template>
                 </div>
             </div>
         </div><!-- end game details -->
